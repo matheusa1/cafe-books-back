b6df0230803f4bbed98cd1d5dace033c945d8d44
HendrickFS <hendrickfel@gmail.com>
2023-08-28 09:39:18 -0300

    criando admin para usuario e livro

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Book Information', {'fields': ['isbn', 'title', 'author', 'publisher', 'country', 'language', 'year', 'pages', 'price', 'stock']}),
+    ]
+    list_display = ('isbn', 'title', 'author', 'publisher', 'country', 'language', 'year', 'pages', 'price', 'stock')
+
+
+admin.site.register(Book)
+
 
 # Register your models here.
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User information',{
+            'fields':['type','id','name','email','password','birth_date','phone','sex','address']
+        })
+    ]
+    list_display = ('type','id','name','email','password','birth_date','phone','sex','address')
+
+admin.site.register(User)
 
 # Register your models here.

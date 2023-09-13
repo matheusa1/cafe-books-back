01b855919ef4dda4f503ddf33f4aeda152f284ca
LuizAbudi <luizabudi@hotmail.com>
2023-09-12 22:20:13 -0300

    adicionando viewsets para o user

---
+++ user/api/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from user.api import serializers
+from user import models
+
+class UserViewsSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserSerializer
+    queryset = models.User.objects.all()
+    

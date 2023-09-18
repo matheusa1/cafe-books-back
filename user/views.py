44ae277bbde32409b8731c19e9b4095c65074141
HendrickFS <hendrickfel@gmail.com>
2023-09-18 09:10:47 -0300

    hash para senha e verificações

--- user/views.py
+++ user/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from .models import User
+from .api.serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import status
 
-# Create your views here.
+from hashlib import md5
+
+class UserAPIView(APIView):
+    def get(self, request):
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        if(len(request.data['password']) < 8):
+            return Response({
+                'error': True,
+                'message': 'A senha deve conter no mínimo 8 caracteres!'
+            }, status=status.HTTP_409_CONFLICT)
+
+        password = request.data['password'].encode('utf-8')
+        request.data['password'] = md5(password).hexdigest()
+        serializer = UserSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'error': False,
+                'message': 'Usuário cadastrado com sucesso!'
+            }, status=status.HTTP_201_CREATED)
+        
+        if serializer.errors:
+            if(User.objects.filter(email=request.data['email']).exists()):
+                return Response({
+                    'error': True,
+                    'message': 'Este e-mail já está cadastrado!'
+                }, status=status.HTTP_409_CONFLICT)
+            if (request.data['name'] == '' or request.data['email'] == '' or request.data['password'] == '' or request.data['sex'] == '' or request.data['type'] == ''):
+                return Response({
+                    'error': True,
+                    'message': 'Todos os campos são obrigatórios!'
+                }, status=status.HTTP_409_CONFLICT)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

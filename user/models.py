3811218bf0bf8475c995a2b4efaaccebef68b2ea
HendrickFS <hendrickfel@gmail.com>
2023-09-22 14:06:58 -0300

    removendo choices

--- user/models.py
+++ user/models.py
@@ -13,14 +13,14 @@ class User(models.Model):
         ('Outro', 'outro'), 
     )
 
-    type = models.CharField(max_length=5, choices=USER_TYPE_CHOICES, default='User')
+    type = models.CharField(max_length=5, default='user')
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     birth_date = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=15, null=True, blank=True)
-    sex = models.CharField(max_length=9, choices=SEX_CHOICE, null=True, blank=True)
+    sex = models.CharField(max_length=9, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
 
     def __str__(self):

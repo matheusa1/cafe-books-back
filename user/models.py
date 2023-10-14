b68a9357d53692a213f78ac146492ea3ecb270ba
HendrickFS <hendrickfel@gmail.com>
2023-10-13 22:46:45 -0300

    atributos adicionados

--- user/models.py
+++ user/models.py
@@ -11,6 +11,8 @@ class Purchase(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     total = models.FloatField()
     books = models.ManyToManyField(Book, through='PurchaseItem')
+    address = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=20, default='Pendente')
 
     def __str__(self):
         return self.user.name + ' - ' + str(self.date)

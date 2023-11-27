5d31769044726fb215807d99155fe852b18f057d
HendrickFS <hendrickfel@gmail.com>
2023-11-26 22:15:43 -0300

    esqueci uma virgula

--- backend/urls.py
+++ backend/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('api/user/purchase/', GetPurchaseByUser.as_view()),
     path('api/purchase/withoutcart/', PurchaseWithoutCartAPIView.as_view()),
     path('api/cart/', CartAPIView.as_view()),
-    path('api/cart/multiple/'), PurchaseItemAPIView.as_view()
+    path('api/cart/multiple/'), PurchaseItemAPIView.as_view(),
     path('api/favorites/', FavoritesAPIView.as_view()),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
